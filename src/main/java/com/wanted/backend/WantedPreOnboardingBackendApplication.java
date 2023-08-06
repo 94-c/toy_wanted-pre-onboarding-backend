c5654812ab6635ed9d00c0eccb70031a0a01f57b
94-c <hyeongwoo26@gmail.com>
2023-08-06 23:57:26 +0900

    # 루트 Application 수정

--- src/main/java/com/wanted/backend/WantedPreOnboardingBackendApplication.java
+++ src/main/java/com/wanted/backend/WantedPreOnboardingBackendApplication.java
@@ -1,11 +1,8 @@
 package com.wanted.backend;
 
-import com.wanted.backend.security.JwtAuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
