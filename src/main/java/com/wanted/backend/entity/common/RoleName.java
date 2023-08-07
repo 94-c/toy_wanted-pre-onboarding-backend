7a79a3a8fc8c0d994c43bb665c4009818b30d5fe
94-c <hyeongwoo26@gmail.com>
2023-08-08 01:14:10 +0900

    # application.yaml 파일 수정 & 전체 리팩토링

--- src/main/java/com/wanted/backend/entity/common/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.wanted.backend.entity.common;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- src/main/java/com/wanted/backend/service/CustomUserDetailService.java
+++
@@ -1,9 +0,0 @@
-package com.wanted.backend.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface CustomUserDetailService {
-    UserDetails loadUserByUsername(String email);
-    UserDetails loadUserById(Long id);
-
-}
