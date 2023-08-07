af88a5bdaad27bd4cf03804c7cba85d9f2065aba
94-c <hyeongwoo26@gmail.com>
2023-08-08 01:16:07 +0900

    # entity 수정

--- src/main/java/com/wanted/backend/entity/User.java
+++ src/main/java/com/wanted/backend/entity/User.java
@@ -1,53 +1,89 @@
 package com.wanted.backend.entity;
 
 import com.wanted.backend.entity.common.DateField;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
-@ToString(callSuper = true)
 @Table(name = "users",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})}
 )
 @Entity
-public class User extends DateField {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends DateField implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Setter
-    @Column(nullable = false, length = 100)
     private String email;
-    @Setter
-    @Column(nullable = false)
     private String password;
-    @Setter
     private String name;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "user_role",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-    )
-    @Setter
-    private Set<Role> roles = new HashSet<>();
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "user_role")
+    private List<String> roles;
 
-    public User() {
+    public static User createNormalMember(String email, String password, String name) {
+        ArrayList<String> roles = new ArrayList<>();
+        roles.add("USER");
+        return new User(email, password, name, roles);
     }
 
-    public User(String email, String password, String name) {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User(String email, String password, String name, List<String> roles) {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.roles = roles;
     }
 
-    public static User of(String email, String password, String name) {
-        return new User(email, password, name);
+    public static User of(String email, String password, String name, List<String> roles) {
+        return new User(email, password, name, roles);
     }
 
     @Override
--- src/main/java/com/wanted/backend/entity/common/DateField.java
+++ src/main/java/com/wanted/backend/entity/common/DateField.java
@@ -1,7 +1,10 @@
 package com.wanted.backend.entity.common;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,7 +16,10 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(
         value = { "createdAt", "updatedAt" },
