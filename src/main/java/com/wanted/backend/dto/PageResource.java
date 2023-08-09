45a8551c659c4bdaa94c6e6938a8832cf921c1ca
94-c <hyeongwoo26@gmail.com>
2023-08-09 16:05:13 +0900

    # Paging 처리 관련한 클래스 생성

--- src/main/java/com/wanted/backend/dto/PageResource.java
+++ src/main/java/com/wanted/backend/dto/PageResource.java
@@ -1,14 +1,11 @@
 package com.wanted.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PageResource<T> {
