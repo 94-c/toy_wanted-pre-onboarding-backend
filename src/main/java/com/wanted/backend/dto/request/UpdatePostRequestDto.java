a3b28ef8e3b1a858f56424bffe07b7575ecd0877
94-c <hyeongwoo26@gmail.com>
2023-08-09 16:05:58 +0900

    # UpdatePostRequestDto 생성

--- src/main/java/com/wanted/backend/dto/request/UpdatePostRequestDto.java
+++ src/main/java/com/wanted/backend/dto/request/UpdatePostRequestDto.java
@@ -1,4 +1,14 @@
 package com.wanted.backend.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdatePostRequestDto {
+    private String title;
+    private String content;
 }
